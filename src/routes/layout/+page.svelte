0eba5ce7c6e8ed9a48f22972e122f1341faec48c
ferdous ara <lailyakter138@gmail.com>
2023-09-04 12:44:12 +0600

    responsive update

--- src/routes/layout/+page.svelte
+++ src/routes/layout/+page.svelte
@@ -4,11 +4,11 @@
 
 
 <div class="container lg-auto px-4">
-    <div class="grid grid-cols-12  grid-rows-6">
-        <div class="bg-blue-400 col-span-2 row-span-3">3X2</div>
-        <div class="bg-rose-100  col-span-4 row-span-5">5X4</div>
-        <div class="bg-blue-400 col-span-6 row-span-2">2X6</div>
-        <div class="bg-yellow-100 col-span-4 row-span-4">4X4
+    <div class="grid lg:grid-cols-12  grid-rows-6 sm:grid-cols-4">
+        <div class="bg-blue-400 lg:col-span-2 lg:row-span-3 sm:hidden lg:block ">3X2</div>
+        <div class="bg-rose-100  lg:col-span-4 lg:row-span-5 sm:col-span-4 sm:row-span-3">5X4</div>
+        <div class="bg-blue-400 lg:col-span-6 lg:row-span-2 sm:hidden lg:block">2X6</div>
+        <div class="bg-yellow-100 lg:col-span-4 lg:row-span-4 sm:col-span-4 sm:row-span-3">4X4
         <div class="mountain-sticker">
             <div class="mountain-inner bg-yellow-100">
                 <div class="mountain">
@@ -16,9 +16,9 @@
             </div>
         </div>
         </div>
-        <div class="bg-emerald-400 col-span-2 row-span-4">4X2</div>
-        <div class="bg-teal-400 col-span-2 row-span-2 ">2X2</div>
-        <div class="bg-fuchsia-400 col-span-6 row-span-1 ">1X6</div>
+        <div class="bg-emerald-400 lg:col-span-2 lg:row-span-4 sm:hidden lg:block">4X2</div>
+        <div class="bg-teal-400 lg:col-span-2 lg:row-span-2 sm:hidden lg:block">2X2</div>
+        <div class="bg-fuchsia-400 lg:col-span-6 lg:row-span-1 sm:hidden lg:block">1X6</div>
     </div>
 </div>
 
